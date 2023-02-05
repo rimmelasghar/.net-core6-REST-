7cc3cbd9d8aee4b959034458fa6463f80b553251
rimmelasghar <rimmelasghar3@gmail.com>
2023-02-05 19:10:33 +0500

    new changes

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public CourseController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            DataTable dt = new Course().Get_All(_configuration);
+            return new JsonResult(dt);
+        }
+    }
+}
---
+++ Models/Course.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TodoApi.Models
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string CourseName { get; set; }
+        public float CourseFee { get; set; }
+        public string CreditHour { get; set; }
+        public string CourseCode { get; set; }
+        public int Status { get; set; }
+
+        public DataTable Get_All(IConfiguration configuration)
+        {
+            string query = @"SELECT * FROM tbl_Course WHERE Status = 1 ORDER BY 1 DESC";
+            DataTable dt = new DataTable();
+            SqlDataReader sqlDataReader;
+            SqlConnection myCon = new SqlConnection(configuration.GetConnectionString("AttendanceAppCon"));
+            using (myCon)
+            {
+                myCon.Open();
+                SqlCommand sc = new SqlCommand(query, myCon);
+                using (sc)
+                {
+                    sqlDataReader = sc.ExecuteReader();
+                    dt.Load(sqlDataReader);
+                    sqlDataReader.Close();
+                    myCon.Close();
+
+                }
+            }
+            return dt;
+        }
+    }
+}
